b8c42b285aee5354e862f7d6b47820e7456ed159
Michal Knobel <michal.knobel@gmail.com>
2016-02-25 17:10:36 +0100

    java project initiated

---
+++ src/main/java/com/gittest/test/tools/App.java
@@ -0,0 +1,11 @@
+package com.gittest.test.tools;
+
+/**
+ * Created by chsklm on 25.02.2016.
+ */
+
+public class App {
+  public final static void main(String[] args) {
+    System.out.println("Test");
+  }
+}
