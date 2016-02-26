7b85d410bcf29f35b9c6073e0a38de212b3c9a6d
Tode Ristov <tode.ristov@seavus.com>
2016-02-26 15:10:02 +0100

    Tode's changes

--- src/main/java/com/gittest/test/tools/App.java
+++ src/main/java/com/gittest/test/tools/App.java
@@ -12,5 +12,6 @@ public class App {
     System.out.println("Change 4");
     System.out.println("Change 4.1");
     System.out.println("Change 4.2");
+    System.out.println("Tode's changes");
   }
 }
