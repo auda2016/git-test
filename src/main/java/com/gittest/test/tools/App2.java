08f53c0ec3c20384bfc04e0bc4568f4d4e0325e7
Michal Knobel <michal.knobel@gmail.com>
2016-02-26 14:54:32 +0100

    App2.java added

---
+++ src/main/java/com/gittest/test/tools/App2.java
@@ -0,0 +1,14 @@
+package com.gittest.test.tools;
+
+
+
+/**
+ * Created by chsklm on 26.02.2016.
+ */
+
+public class App2 {
+
+  public final static void main(String[] args) {
+    System.out.print("michal");
+  }
+}
