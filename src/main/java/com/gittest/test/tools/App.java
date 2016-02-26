e9899c0d778514baf1c37cb8a6f5eb34680fc16c
ristevskiandrej <andrej.ristevski@seavus.com>
2016-02-26 14:55:46 +0100

    Andrej change

--- src/main/java/com/gittest/test/tools/App.java
+++ src/main/java/com/gittest/test/tools/App.java
@@ -11,8 +11,12 @@ public class App {
     System.out.println("Change 3");
     System.out.println("Change 4");
     System.out.println("Change 4.1");
+
     System.out.println("Change 4.2");
     System.out.println("Andrej");
       
+
+    System.out.println("Ivo");
+
   }
 }
